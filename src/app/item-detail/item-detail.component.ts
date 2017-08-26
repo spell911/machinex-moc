e883b79c70bcc930898e1e1ab572c8c7253d5669
nekoten01 <nekoten_april@hotmail.com>
2017-08-27 01:15:35 +0700

    update itemdetail component add product detail and company infomation tabs

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -19,6 +19,8 @@ export class ItemDetailComponent implements OnInit {
       let itemIdR = params["cateName"];
       this.itemId = itemIdR;
     });
+
+    console.log('hello there! '+ this.itemId);
   }
 
 }

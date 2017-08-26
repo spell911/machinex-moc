d47e254d078d3dd002a21b4a8876de1bfb53da94
nekoten01 <nekoten_april@hotmail.com>
2017-08-26 12:05:41 +0700

    push item detail again

---
+++ src/app/item-detail/item-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.component.html',
+  styleUrls: ['./item-detail.component.css']
+})
+export class ItemDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  collapsClick(){
+    
+  }
+
+}

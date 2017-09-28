75233b3f8bb06d6383c160321df3643e425758de
spell911 <spell911@hotmail.co.th>
2017-09-28 21:46:08 +0700

    add npm install flag-icon-css and side nav

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef } from '@angular/core';
+declare var jQuery: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
 
   ngOnInit() {
+    jQuery(this.el.nativeElement).find('.button-collapse').sideNav();
   }
 
 }
